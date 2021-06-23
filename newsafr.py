289771135d9ba660f9391d81ea1bdf4f89b4290c
alexgolovko <ya.golovko-alex2017@yandex.ru>
2021-06-23 09:39:45 +0300

    доработанная программа для получения 10 популярных слов в описании

--- newsafr.py
+++ newsafr.py
@@ -1,12 +1,18 @@
 import collections
-import json
+import xml.etree.ElementTree as ET
 
-def read_json(file_path, max_len_word=6, top_word=10):
-    with open(file_path, encoding='utf-8') as news_file:
-        news = json.load(news_file)
-        description_word = []
-        for item in news['rss']['channel']['items']:
-            description = [word for word in item['description'].split(' ') if len(word) > max_len_word]
-            description_word.extend(description)
-            counter_words = collections.Counter(description_word)
-        print(counter_words.most_common(top_word))
+def read_xml(file_path, max_len_word=6, top_word=10):
+    parser = ET.XMLParser(encoding='utf-8')
+    tree = ET.parse(file_path, parser)
+    root = tree.getroot()
+    xml_title = root.findall('channel/item/description')
+    description_words = []
+    for title in xml_title:
+        description = [word for word in title.text.split(' ') if len(word) > max_len_word]
+        description_words.extend(description)
+        counter_words = collections.Counter(description_words)
+    return counter_words.most_common(top_word)
+
+
+
+print(read_xml('newsafr.xml'))
